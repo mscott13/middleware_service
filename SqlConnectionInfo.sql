c2e3196f07c3957918a7b131716b5ee51c5aea7c
unknown <a.markscott13@gmail.com>
2019-09-15 22:31:59 -0500

    update

---
+++ SqlConnectionInfo.sql
@@ -0,0 +1,13 @@
+﻿SP_WHO2
+
+select * from sys.configurations
+where name ='user connections'
+
+SELECT oPC.cntr_value AS connection_count
+FROM sys.dm_os_performance_counters oPC
+WHERE 
+	(
+		oPC.[object_name] = 'SQLServer:General Statistics'
+			AND oPC.counter_name = 'User Connections'
+	)
+ORDER BY 1;
--- middleware_service/middleware_service/Constants.cs
+++ middleware_service/middleware_service/Constants.cs
@@ -4,10 +4,10 @@
     {
         public static string DB_GENERIC_NAME = "ASMSGenericMaster";
         public static string DB_INTEGRATION_NAME = "ASMSSAGEINTEGRATION";
-        public static string DB_GENERIC = @"Data Source=ERP-SRVR\ASMSDEV;Initial Catalog="+ DB_GENERIC_NAME + ";Integrated Security=True; MultipleActiveResultSets=true";
-        public static string DB_INTEGRATION = @"Data Source=ERP-SRVR\ASMSDEV;Initial Catalog="+DB_INTEGRATION_NAME+ ";Integrated Security=True; MultipleActiveResultSets=true";
-        public static string TEST_DB_GENERIC = @"Data Source=SERVER-ERP2\ASMSDEV;Initial Catalog=" + DB_GENERIC_NAME + ";Integrated Security=True; MultipleActiveResultSets=true";
-        public static string TEST_DB_INTEGRATION = @"Data Source=SERVER-ERP2\ASMSDEV;Initial Catalog=" + DB_INTEGRATION_NAME + ";Integrated Security=True; MultipleActiveResultSets=true";
+        public static string DB_GENERIC = @"Data Source=ERP-SRVR\ASMSDEV;Initial Catalog="+ DB_GENERIC_NAME + ";Integrated Security=True; MultipleActiveResultSets=true; Connection Timeout=30; MinPoolSize=20";
+        public static string DB_INTEGRATION = @"Data Source=ERP-SRVR\ASMSDEV;Initial Catalog="+DB_INTEGRATION_NAME+ ";Integrated Security=True; MultipleActiveResultSets=true; Connection Timeout=30; MinPoolSize=20";
+        public static string TEST_DB_GENERIC = @"Data Source=SERVER-ERP2\ASMSDEV;Initial Catalog=" + DB_GENERIC_NAME + ";Integrated Security=True; MultipleActiveResultSets=true; Connection Timeout=30; MinPoolSize=20";
+        public static string TEST_DB_INTEGRATION = @"Data Source=SERVER-ERP2\ASMSDEV;Initial Catalog=" + DB_INTEGRATION_NAME + ";Integrated Security=True; MultipleActiveResultSets=true; Connection Timeout=30; MinPoolSize=20";
 
         public static int PORT = 8080;
         public static string BASE_ADDRESS = "http://*:";
